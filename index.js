57341e7d77121c1c69acf5652539baece3c6141c
jehfkemsy <jehf.denezaire@gmail.com>
2022-11-25 14:11:22 -0800

    added folders

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 console.log("Hello there. General Kenobi!");
+
+const sidebarEl = document.getElementById("folders");
+const contentEl = document.getElementById("content");
+
+console.log(sidebarEl, contentEl);
+
+new Array(1000).fill(null).map((v, i) => {
+  const el = document.createElement("div");
+  el.className = "folder-label";
+
+  const img = document.createElement("img");
+  img.src = "icons8-folder-60.png";
+  img.className = "folder-icon";
+
+  const p = document.createElement("p");
+  p.innerText = `Folder ${i + 1}`;
+
+  el.appendChild(img);
+  el.appendChild(p);
+
+  sidebarEl.appendChild(el);
+});
